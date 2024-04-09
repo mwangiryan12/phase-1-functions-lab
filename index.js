02e4e4cbc6222c4832cb6c3ca527ac4fbbb69a4f
mwangiryan12 <mwangiryan12@gmail.com>
2024-04-09 10:13:47 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+    const  locationHq = 42
+    return Math.abs(somevalue-locationHq)
+ }
+ function distanceFromHqInFeet(somevalue){
+     return distanceFromHqInBlocks(somevalue)*264;
+ }
+ function  distanceTravelledInFeet(start,destination){
+     return Math.abs(destination-start)*264
+ }
+ function calculatesFarePrice(start,destination){
+     const distance = distanceTravelledInFeet(start,destination);
+     if(distance<400){
+         return 0;
+     }else if(distance>400 && distance<=2000){
+         return(distance-400)*0.02
+     }else if(distance>2000 && distance<2500){
+         return 25;
+     }else if(distance>2500){
+         return 'cannot travel that far'
+     }
+ }
+ 
